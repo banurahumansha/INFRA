4f32fa1e060f81f18ee607d4a37c32c67e231655
Banurahuman <141429927+Banurahuman@users.noreply.github.com>
2026-07-20 18:41:18 +0800

    updated hero carousel

--- assets/js/about.js
+++ assets/js/about.js
@@ -1,17 +1,3 @@
-fetch("components/navbar.html")
-    .then(response => response.text())
-    .then(data => {
-        document.getElementById("navbar").innerHTML = data;
-         initNavbar();
-    });
-// Footer
-fetch("components/footer.html")
-    .then(response => response.text())
-    .then(data => {
-
-        document.getElementById("footer").innerHTML = data;
-
-    });
 
 // Capablities
 
--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,3 +1,18 @@
+fetch("components/navbar.html")
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById("navbar").innerHTML = data;
+         initNavbar();
+    });
+// Footer
+fetch("components/footer.html")
+    .then(response => response.text())
+    .then(data => {
+
+        document.getElementById("footer").innerHTML = data;
+
+    });
+
 function initNavbar() {
 
     /*==============================
